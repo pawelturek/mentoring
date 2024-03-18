60b772b93fc7e98c9532ff24e417bc5f6edc0da1
Turek <turekp@emea.roche.com>
2024-03-18 14:48:15 +0100

    classes added with private method

--- AOC2016Day4Classes.py
+++ AOC2016Day4Classes.py
@@ -1,86 +1,107 @@
+import os.path
 import string
 
-#todo
+sector_sum = []
 class EncryptedData:
-
+    """loading encrypted data from txt file"""
 
     def __init__(self, file_txt: str):
-        with open(file_txt, "r") as f:
-            while True:
-                return [line.strip() for line in f.readlines()]
+        self.lines_object_list = []
+        self.file_txt = file_txt
+        self.local_dir = r'C:\Users\turekp\Desktop\mentoring\mentoring'
+        self.file_path = os.path.join(self.local_dir, self.file_txt)
+        print(self.file_path)
+
+        try:
+            with open(self.file_path, "r") as f:
+                for line in f.readlines():
+                    r = Room(line.strip())
+                    self.lines_object_list.append(r)
+        except IOError:
+            raise IOError("{} not found in {}".format(self.file_txt, self.local_dir))
 
 
-def count_letters2(code: str):
-    word_list = []
-    one_word: int = code.find('-')
-    word_list.append(code[0:one_word])
-    while True:
-        second_word = code.find('-', one_word + 1, len(code))
-        if any(char.isdigit() for char in code[one_word + 1:second_word]):
-            second_word = code.find('[', one_word + 1, len(code))
-            sector_id = code[one_word + 1:second_word]
+
+class Room:
+
+
+    def __init__(self, line: str):
+        self.occurrences_dict = {q: 0 for q in string.ascii_lowercase[:26]}
+        self.line = line
+        self.word_list = []
+        self.checksum_calculated = ''
+        self.current_checksum = ''
+        self.checksum_calc = ''
+        self.occurrences_dict_to_sort = dict()
+        self.occurrence_check = {}
+        self.sector_id = 0
+        self.checksum = ''
+        self.__parse_data()
+
+
+    def __parse_data(self):
+        one_word: int = self.line.find('-')
+        self.word_list.append(self.line[0:one_word])
+        while True:
+            second_word = self.line.find('-', one_word+1, len(self.line))
+            if any(char.isdigit() for char in self.line[one_word + 1:second_word]):
+                second_word = self.line.find('[', one_word + 1, len(self.line))
+                self.sector_id = int(self.line[one_word + 1:second_word])
+                one_word = second_word
+                second_word = self.line.find(']', one_word + 1, len(self.line))
+                self.checksum = self.line[one_word + 1:second_word]
+                break
+            self.word_list.append(self.line[one_word + 1:second_word])
             one_word = second_word
-            second_word = code.find(']', one_word + 1, len(code))
-            checksum = code[one_word + 1:second_word]
-            break
-        word_list.append(code[one_word + 1:second_word])
-        one_word = second_word
-    return word_list, sector_id, checksum
 
+ #   def __str__(self):
+ #       return "word list: "+ str(self.word_list)+" sector id: "+ self.sector_id + " checksum :"\
+ #               + self.checksum
 
-def number_of_occurrences(word_list: list):
-    occurrences_dict = dict()
-    for q in string.ascii_lowercase[:26]:
-        occurrences_dict[q] = 0
-    for w in word_list:
-        for i in w:
-            occurrences_dict[i] = occurrences_dict[i] + 1
-    return occurrences_dict
+    def number_of_occurrences(self, word_list: list):
+        # occurrences_dict = dict()
+        self.occurrences_dict = {q: 0 for q in string.ascii_lowercase[:26]}
+        for w in word_list:
+             for i in w:
+                self.occurrences_dict[i] = self.occurrences_dict[i] + 1
+        return self.occurrences_dict
 
+    def occurrences_sort(self, occurrences_dict_to_sort: dict):
+        self.occurrences_dict_to_sort = occurrences_dict_to_sort
+        alphabetic = {key: value for key, value in sorted(self.occurrences_dict_to_sort.items())}
+        sorted_dict = {key: value for key, value in sorted(alphabetic.items(), key=lambda x: x[1], reverse=True)}
+        return sorted_dict
 
-def occurrences_sort(occurrences_dict: dict):
-    alphabetic = {key: value for key, value in sorted(occurrences_dict.items())}
-    sorted_dict = {key: value for key, value in sorted(alphabetic.items(), key=lambda x: x[1], reverse=True)}
-    return sorted_dict
+    def calculate_checksum(self, occurrence_check: dict):
+        self.occurrence_check = occurrence_check
+        checksum_list = []
+        max_checksum = 0
+        for i in self.occurrence_check.keys():
+            if self.occurrence_check[i] > 0 and max_checksum < 5:
+                checksum_list.append(i)
+                max_checksum += 1
+        self.checksum_calculated = ''.join(checksum_list)
+        return self.checksum_calculated
 
-
-def calculate_checksum(occurrence_check: dict):
-    checksum_list = []
-    max_checksum = 0
-    for i in occurrence_check.keys():
-        if occurrence_check[i] > 0 and max_checksum < 5:
-            checksum_list.append(i)
-            max_checksum += 1
-    checksum = ''.join(checksum_list)
-    return checksum
-
-
-
-def is_room_real(checksum_calculated: str, current_checksum: str, sector_id: int) -> int:
-    if current_checksum == checksum_calculated:
-        return int(sector_id)
-    else:
-        return 0
+    def is_room_real(self, checksum_calc: str, current_checksum: str, sector_id: int) -> int:
+        self.checksum_calc = checksum_calc
+        self.current_checksum = current_checksum
+        self.sector_id = sector_id
+        global sector_sum
+        if self.current_checksum == self.checksum_calc:
+            sector_sum.append(int(sector_id))
+            return int(sector_id)
+        else:
+            return 0
 
 
 def main():
-    total_sum = 0
-    for line in range(0,len(open_file("test.txt"))):
-        print('Is room real: ', is_room_real(calculate_checksum(occurrences_sort(number_of_occurrences(count_letters2(open_file("test.txt")[line])[0]))), count_letters2(open_file("test.txt")[line])[2], count_letters2(open_file("test.txt")[line])[1]))
-        total_sum += is_room_real(calculate_checksum(occurrences_sort(number_of_occurrences(count_letters2(open_file("test.txt")[line])[0]))), count_letters2(open_file("test.txt")[line])[2], count_letters2(open_file("test.txt")[line])[1])
-    return total_sum
+    e = EncryptedData("test.txt")
+    for r in e.lines_object_list:
 
-print('suma sector id', main())
-
+        r.calculate_checksum(r.occurrences_sort(r.number_of_occurrences(r.word_list)))
+        print(r.is_room_real(r.checksum_calculated, r.checksum, r.sector_id))
+    print('sum of sector id for real rooms: ', sum(sector_sum))
 
 
-
-# print('count_letters2: ',count_letters2(open_file("test.txt")[0])[0]) #numer liniijki w pliku, 0 - word list, 1 - sector, 2 checksum, nr slowa
-# print('number_of_occurrences: ',number_of_occurrences(count_letters2(open_file("test.txt")[0])[0][0]))
-# print('aaaaaaaaaa')
-# print(occurrences_sort(number_of_occurrences(count_letters2(open_file("test.txt")[0])[0])))
-# print('number_of_occurrences: ',number_of_occurrences(count_letters2(open_file("test.txt")[0])[0]))
-# print('sorted: ',occurrences_sort(number_of_occurrences(count_letters2(open_file("test.txt")[0])[0])))
-# print(calculate_checksum(occurrences_sort(number_of_occurrences(count_letters2(open_file("test.txt")[0])[0]))))
-
-#print('czek: ',count_letters2(open_file("test.txt")[0])[1])
+main()
